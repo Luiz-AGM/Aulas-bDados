483a841fa75c29558eb2c21a503bbe959fdd44f9
Luiz-AGM <luiz_agm@hotmail.com>
2022-05-13 10:28:14 -0300

    atts ok

---
+++ example-app/pages/api/movies.js
@@ -0,0 +1,21 @@
+import connectToDatabase from "../../lib/mongodb";
+
+export default async function (req, res) {
+    const db = await connectToDatabase();
+
+    // GET
+    if (req.method == "GET") {
+        const movies = await db.collection("movies")
+            .find({ year: 2010 })
+            .sort({ title: 1 })
+            .toArray();
+        
+        res.json(movies);
+    }
+
+    // POST
+
+    // PUT
+
+    // DELETE
+}
